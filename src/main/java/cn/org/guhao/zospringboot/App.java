e8df64507c03062e7f605e724882e18e5c71b496
guhao <dagmom@163.com>
2015-04-29 10:22:52 +0800

    项目初始化

---
+++ src/main/java/cn/org/guhao/zospringboot/App.java
@@ -0,0 +1,13 @@
+package cn.org.guhao.zospringboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
